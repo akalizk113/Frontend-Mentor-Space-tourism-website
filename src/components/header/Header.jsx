025a23139cd20c9ac3d87c20c8b6c39fb39d0163
Pham Tuan Anh <akalizk17@gmail.com>
2021-11-07 12:42:39 +0700

    responsive table + mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,12 @@
 import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+
+import { useState } from 'react';
 import './Header.scss';
 
 import { navItems } from '../../constants';
 
 const Header = () => {
+   const [checked, setChecked] = useState(false);
    return (
       <header className="header">
          <svg
@@ -20,10 +23,43 @@ const Header = () => {
                />
             </g>
          </svg>
+
+         <input
+            type="checkbox"
+            id="nav-mobile-checkbox"
+            name="nav-mobile-checkbox"
+            checked={checked}
+            onChange={() => setChecked(!checked)}
+         />
+         <label htmlFor="nav-mobile-checkbox">
+            <svg
+               className="nav-moblie-close-icon"
+               xmlns="http://www.w3.org/2000/svg"
+               width="20"
+               height="21"
+            >
+               <g fill="#D0D6F9" fillRule="evenodd">
+                  <path d="M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z" />
+                  <path d="M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z" />
+               </g>
+            </svg>
+
+            <svg
+               className="nav-moblie-menu-icon"
+               xmlns="http://www.w3.org/2000/svg"
+               width="24"
+               height="21"
+            >
+               <g fill="#D0D6F9" fillRule="evenodd">
+                  <path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z" />
+               </g>
+            </svg>
+         </label>
+
          <ul className="nav">
             {navItems.map((item, index) => (
                <li key={index} className="nav__item">
-                  <CustomLink to={item.path}>
+                  <CustomLink onClick={() => setChecked(false)} to={item.path}>
                      <span>{index < 10 ? '0' + index : index}</span>
                      {item.label}
                   </CustomLink>
